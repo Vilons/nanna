f904d5cb21b9431b03adbbf7454009dafe0a6cdf
Vilons <104717752+Vilons@users.noreply.github.com>
2022-05-02 22:10:47 +0300

    Update nick.lua

--- nick.lua
+++ nick.lua
@@ -1,1 +1,1 @@
-frozengun223 FrozenGun223 1ytertwr rmkmeirzh santeeehnik89@gmail.com 123wwegef Enlesss enlesss sayn 1231yfw
+wisl21 frozengun223 FrozenGun223 1ytertwr rmkmeirzh santeeehnik89@gmail.com 123wwegef Enlesss enlesss sayn 1231yfw
